86a9fe77a032637e386ddf37b00e7d376b1bec9d
Jonas Carpay <jonascarpay@gmail.com>
2019-10-02 22:05:11 +0900

    Fix nix

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {}, ... }:
+{ pkgs ? import <nixpkgs> {} }:
 let
   grid-label-deriv =
     { mkDerivation, base, bytestring, containers, filepath, gloss
@@ -6,7 +6,7 @@ let
     }:
     mkDerivation {
       pname = "grid-label";
-      version = "0.1.0.0";
+      version = "0.1";
       src = ./.;
       isLibrary = false;
       isExecutable = true;
@@ -14,20 +14,20 @@ let
         base bytestring containers filepath gloss JuicyPixels
         optparse-applicative vector
       ];
-      homepage = "https://github.com/githubuser/grid-label#readme";
+      homepage = "https://github.com/xc-jp/grid-label#readme";
       license = stdenv.lib.licenses.bsd3;
     };
   grid-label = pkgs.haskellPackages.callPackage grid-label-deriv {};
 in
-  rec {
+  {
     grid-label-nix = grid-label;
     grid-label-linux = grid-label.overrideAttrs (old: {
+      name = old.name + "-linux";
       buildInputs = with pkgs; old.buildInputs ++ [ file patchelf ];
       fixupPhase = ''
         file $out/bin/grid-label
         patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 $out/bin/grid-label 
         patchelf --set-rpath /lib:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu $out/bin/grid-label 
-        echo $out
       '';
     });
   }
