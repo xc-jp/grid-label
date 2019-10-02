05115c39d6ad068b504adfd217d896b5c966b945
Jonas Carpay <jonascarpay@gmail.com>
2019-10-02 21:07:31 +0900

    Fix build to build nix and linux binaries

--- default.nix
+++ default.nix
@@ -1,41 +1,33 @@
-{pkgs ? import (builtins.fetchGit {
-  name = "nixos-release-19.03-2019-09-05";
-  url = https://github.com/nixos/nixpkgs.git;
-  ref = "release-19.03";
-  rev = "3d7608eb2673dee9567ddbadab1356444f16c41e";
-  }) {
-    overlays = [
-      (self: super: {
-        libGL = self.callPackage ./mesa-stubs.nix {
-          inherit (self) OpenGL mesa;
-        };
-        mesa = self.mesa_noglu;
-        gmp = super.gmp.override {
-          withStatic = true;
-        };
-        freeglut = super.freeglut.overrideAttrs(old: {
-            nativeBuildInputs = [super.cmake];
-        });
-        mesa_noglu = ((super.mesa_noglu.override
-            { vulkanDrivers = [];
-              galliumDrivers = ["virgl" "nouveau" "vc4" "swrast"];
-              driDrivers = ["nouveau"];
-            }
-          ).overrideAttrs(old: {
-            nativeBuildInputs = old.nativeBuildInputs ++ [super.llvm_7];
-          }));
-        
-        xorg = super.xorg // {
-          libXi = super.xorg.libXi.overrideAttrs(old: {
-            buildInputs = old.buildInputs ++ [super.autoconf super.automake];
-            patches = [./libXi-cross-musl.patch];
-          });
-        };
-        grid-label = super.haskell.packages.ghc865.developPackage {
-          root = ./.;
-        };
-      })
-    ];
+{ pkgs ? import <nixpkgs> {}, ... }:
+let
+  grid-label-deriv =
+    { mkDerivation, base, bytestring, containers, filepath, gloss
+    , JuicyPixels, optparse-applicative, stdenv, vector
+    }:
+    mkDerivation {
+      pname = "grid-label";
+      version = "0.1.0.0";
+      src = ./.;
+      isLibrary = false;
+      isExecutable = true;
+      executableHaskellDepends = [
+        base bytestring containers filepath gloss JuicyPixels
+        optparse-applicative vector
+      ];
+      homepage = "https://github.com/githubuser/grid-label#readme";
+      license = stdenv.lib.licenses.bsd3;
+    };
+  grid-label = pkgs.haskellPackages.callPackage grid-label-deriv {};
+in
+  rec {
+    grid-label-nix = grid-label;
+    grid-label-linux = grid-label.overrideAttrs (old: {
+      buildInputs = with pkgs; old.buildInputs ++ [ file patchelf ];
+      fixupPhase = ''
+        file $out/bin/grid-label
+        patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 $out/bin/grid-label 
+        patchelf --set-rpath /lib:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu $out/bin/grid-label 
+        echo $out
+      '';
+    });
   }
-}:
-pkgs.pkgsCross.musl64
